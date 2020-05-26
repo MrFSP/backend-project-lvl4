6c55bd6f1748e2041876b4816fa105028bfa80f2
Pavel Egorov <PavelEgorov@MacBookPro-MrFSP.local>
2020-05-26 17:59:00 +0300

    Refactor tests

--- __tests__/app.session.test.js
+++ __tests__/app.session.test.js
@@ -4,25 +4,31 @@ import { promises as fs } from 'fs';
 import path from 'path';
 import faker from 'faker';
 import User from '../server/entity/User';
+import Task from '../server/entity/Task';
+import TaskStatus from '../server/entity/TaskStatus';
+import Tag from '../server/entity/Tag';
 import encrypt from '../server/lib/secure.js';
 
 import app from '../server';
 
-const user = {
-  email: faker.internet.email(),
-  password: faker.internet.password(),
-  firstName: faker.name.firstName(),
-  lastName: faker.name.lastName(),
+const currUser = {
+  email: 'MustafaJohns36@hotmail.com',
+  password: 'j1VFGV6z0PQVpQJ',
+  firstName: 'Ottilie',
+  lastName: 'Pagac',
 };
 
 const changedUser = {
-  email: `changed${user.email}`,
-  password: `changed${user.password}`,
-  firstName: `changed${user.firstName}`,
-  lastName: `changed${user.lastName}`,
+  email: `changed${currUser.email}`,
+  password: `changed${currUser.password}`,
+  firstName: `changed${currUser.firstName}`,
+  lastName: `changed${currUser.lastName}`,
 };
 
-const getCookie = async (server) => {
+const newTag = { name: 'Main' };
+const newTaskStatus = { name: 'tests' };
+
+const getCookie = async (server, user) => {
   const res = await request.agent(server.server)
     .post('/session')
     .send({ object: user })
@@ -34,13 +40,24 @@ const getCookie = async (server) => {
 
 const pathToFixtures = path.join(__dirname, '__fixtures__');
 
-let logoutPagetml;
+let logoutPagehtml;
+let settingsPagehtml;
+let afterSignInPagehtml;
+let userPagehtml;
+let newTaskPagehtml;
+let changePassPagehtml;
+let cookie;
 
 describe('Testing session for registered user', () => {
   let server;
 
   beforeAll(async () => {
-    logoutPagetml = await fs.readFile(path.join(pathToFixtures, 'logout-page.html'));
+    logoutPagehtml = await fs.readFile(path.join(pathToFixtures, 'logout-page.html'));
+    settingsPagehtml = await fs.readFile(path.join(pathToFixtures, 'settings-page.html'));
+    afterSignInPagehtml = await fs.readFile(path.join(pathToFixtures, 'after-sign-in-page.html'));
+    userPagehtml = await fs.readFile(path.join(pathToFixtures, 'user-page.html'));
+    newTaskPagehtml = await fs.readFile(path.join(pathToFixtures, 'newtask-page.html'));
+    changePassPagehtml = await fs.readFile(path.join(pathToFixtures, 'change-pass-page.html'));
 
     expect.extend(matchers);
     server = app();
@@ -48,7 +65,7 @@ describe('Testing session for registered user', () => {
   });
 
   beforeEach(async () => {
-    await server.ready();
+    // await server.ready();
   });
 
   // afterEach(async () => {
@@ -59,80 +76,87 @@ describe('Testing session for registered user', () => {
     await request.agent(server.server)
       .post('/users')
       .set('Content-Type', 'application/json')
-      .send({ user })
+      .send({ user: currUser })
       .catch((err) => {
         console.log(err);
       });
 
-    const userFromDb = await User.findOne({ where: { email: user.email } });
+    const userFromDb = await User.findOne({ where: { email: currUser.email } });
 
-    expect(userFromDb.email).toEqual(user.email);
-    expect(userFromDb.passwordDigest).toEqual(encrypt(user.password));
+    expect(userFromDb.email).toEqual(currUser.email);
+    expect(userFromDb.passwordDigest).toEqual(encrypt(currUser.password));
   });
 
   it('Should get new session', async () => {
     const res = await request.agent(server.server)
       .post('/session')
-      .send({ object: user })
+      .send({ object: currUser })
       .redirects(2)
       .catch((err) => {
         console.log(err);
       });
-    
+
     expect(res).toHaveHTTPStatus(200);
+    expect(res.text.toString()).toEqual(afterSignInPagehtml.toString());
   });
 
   it('Should return current session', async () => {
     const res = await request.agent(server.server)
       .get('/users/user')
-      .set('cookie', await getCookie(server))
+      .set('cookie', await getCookie(server, currUser))
+      .redirects(1)
       .catch((err) => {
         console.log(err);
       });
-    
+
     expect(res).toHaveHTTPStatus(200);
+    expect(res.text.toString()).toEqual(userPagehtml.toString());
   });
 
-  it('Should get page "create new task"', async () => {
+  it('Should get page "/tasks/new"', async () => {
     const res = await request.agent(server.server)
       .get('/tasks/new')
-      .set('cookie', await getCookie(server))
+      .set('cookie', await getCookie(server, currUser))
       .catch((err) => {
         console.log(err);
       });
     
     expect(res).toHaveHTTPStatus(200);
+    expect(res.text.toString()).toEqual(newTaskPagehtml.toString());
   });
 
-  it('Should get page "task settings"', async () => {
+  it('Should get page "/tasks/settings"', async () => {
     const res = await request.agent(server.server)
       .get('/tasks/settings')
-      .set('cookie', await getCookie(server))
+      .set('cookie', await getCookie(server, currUser))
+      .redirects(1)
       .catch((err) => {
         console.log(err);
       });
     
     expect(res).toHaveHTTPStatus(200);
+    expect(res.text.toString()).toEqual(settingsPagehtml.toString());
   });
 
   it('Should get page for changing password', async () => {
     const res = await request.agent(server.server)
       .get('/users/password')
-      .set('cookie', await getCookie(server))
+      .set('cookie', await getCookie(server, currUser))
       .catch((err) => {
         console.log(err);
       });
-    
+
     expect(res).toHaveHTTPStatus(200);
+    expect(res.text.toString()).toEqual(changePassPagehtml.toString());
   });
 
   it('Should get changed password', async () => {
     await request.agent(server.server)
       .post('/users/password/index')
-      .set('cookie', await getCookie(server))
+      .set('cookie', await getCookie(server, currUser))
       .send({
         object: {
-          oldPass: user.password,
+          oldPass: currUser.password,
           newPass: changedUser.password,
           confirmNewPass: changedUser.password,
         }
@@ -141,7 +165,7 @@ describe('Testing session for registered user', () => {
         console.log(err);
       });
 
-    const userFromDb = await User.findOne({ where: { email: user.email } });
+    const userFromDb = await User.findOne({ where: { email: currUser.email } });
 
     expect(userFromDb.passwordDigest).toEqual(encrypt(changedUser.password));
   });
@@ -155,7 +179,7 @@ describe('Testing session for registered user', () => {
       });
     
     expect(res).toHaveHTTPStatus(200);
-    expect(res.text.toString()).toEqual(logoutPagetml.toString());
+    expect(res.text.toString()).toEqual(logoutPagehtml.toString());
   });
 
   afterAll(async () => {
