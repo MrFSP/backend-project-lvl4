1a789f0307af28c36c4b0631436d922b76e48134
Pavel Egorov <PavelEgorov@MacBookPro-MrFSP.local>
2020-05-26 17:53:12 +0300

    Changed ormconfig.js properties for tests

--- ormconfig.js
+++ ormconfig.js
@@ -1,4 +1,27 @@
-export default {
+const sqlite = {
+  type: 'sqlite',
+  database: `${__dirname}/__tests__/database.sqlite`,
+  synchronize: true,
+  logger: 'debug',
+  logging: true,
+  entities: [
+    `${__dirname}/server/entity/**/*.js`,
+  ],
+  migrations: [
+    'server/migration/*.js',
+  ],
+  subscribers: [
+    'server/subscriber/*.js',
+  ],
+  cli: {
+    entitiesDir: 'server/entity',
+    migrationsDir: 'server/migration',
+    subscribersDir: 'server/subscriber',
+  },
+};
+
+
+const postgre = {
    type: "postgres",
    host: "localhost",
    url: process.env.DATABASE_URL || '',
@@ -23,3 +46,5 @@ export default {
     subscribersDir: 'server/subscriber',
   },
 }
+
+export default process.env.NODE_ENV !== 'test' ? postgre : sqlite;
