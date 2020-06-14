9e166afa0bbfc674f45574b2b91537b91c329559
Pavel Egorov <PavelEgorov@MacBookPro-MrFSP.local>
2020-06-14 12:17:43 +0300

    Route taskstatuses added to controller

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,12 +4,14 @@ import welcome from './welcome';
 import users from './users';
 import session from './session';
 import tasks from './tasks';
+import taskstatuses from './taskstatuses';
 
 const controllers = [
   welcome,
   users,
   session,
-  tasks
+  tasks,
+  taskstatuses
 ];
 
 export default (app) => controllers.forEach((f) => f(app));
