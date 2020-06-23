d6a173ba94bb228c0339dff00b02a357f8545e68
Pavel Egorov <PavelEgorov@MacBookPro-MrFSP.local>
2020-06-23 03:29:06 +0300

    Bugfix

--- ormconfig.js
+++ ormconfig.js
@@ -30,7 +30,7 @@ const postgres = {
    username: process.env.DB_USERNAME,
    password: process.env.DB_PASSWORD,
    database: process.env.DB_NAME,
-   synchronize: true,
+   synchronize: false,
    logging: true,
    entities: [
       `${__dirname}/server/entity/**/*.js`,
